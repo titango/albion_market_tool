ca7ee677c08e1628a18e7a37c583ae16c3d7bf63
Tan Ngo <ngotanthinh92@gmail.com>
2020-11-13 23:40:08 -0800

    Add test nested Dialog

--- src/components/DialogMarketplaceSave.js
+++ src/components/DialogMarketplaceSave.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -25,39 +25,47 @@ const DialogMarketplaceSave = (props) => {
   const classes = useStyles();
   const { onClose, listItems, selectedValue, open } = props;
 
+  const [openAddNew, setOpenAddNew] = useState(false);
+
   const handleClose = () => {
     onClose(selectedValue);
   };
 
   const handleListItemClick = (value) => {
+    setOpenAddNew(true);
     onClose(value);
   };
 
   return(
-    <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
-      <DialogTitle id="simple-dialog-title">Your market list</DialogTitle>
-      <List>
-        {listItems.map((items) => (
-          <ListItem button onClick={() => handleListItemClick(items.name)} key={items.name}>
+    <React.Fragment>
+      <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+        <DialogTitle id="simple-dialog-title">Your market list</DialogTitle>
+        <List>
+          {listItems.map((items) => (
+            <ListItem button onClick={() => handleListItemClick(items.name)} key={items.name}>
+              <ListItemAvatar>
+                <Avatar className={classes.avatar}>
+                  <DescriptionIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={items.name} />
+            </ListItem>
+          ))}
+
+          <ListItem autoFocus button onClick={() => handleListItemClick('addAccount')}>
             <ListItemAvatar>
-              <Avatar className={classes.avatar}>
-                <DescriptionIcon />
+              <Avatar>
+                <AddIcon />
               </Avatar>
             </ListItemAvatar>
-            <ListItemText primary={items.name} />
+            <ListItemText primary="Add account" />
           </ListItem>
-        ))}
-
-        <ListItem autoFocus button onClick={() => handleListItemClick('addAccount')}>
-          <ListItemAvatar>
-            <Avatar>
-              <AddIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Add account" />
-        </ListItem>
-      </List>
-    </Dialog>
+        </List>
+      </Dialog>
+      <Dialog open={openAddNew}>
+          
+      </Dialog>
+    </React.Fragment>
   )
 }
 
